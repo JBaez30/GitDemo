af4c0bfb26d50186ee019899a982566b093f89c4
Jose Baez <baezsantiago30@gmail.com>
2020-09-06 12:34:57 -0400

    First Commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    cout << "This is a Git demo to track recent changes\n";
+
+    system("pause");
+    cout << "\n\n";
+    return 0;
+}
