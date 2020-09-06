99f5fd3d0672248512aaf8769b21544464487860
Jose Baez <baezsantiago30@gmail.com>
2020-09-06 12:41:33 -0400

    Adding an additional line of code to commit

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "Hello World!\n";
 
     cout << "This is a Git demo to track recent changes\n";
+    cout << "Adding a new message to track\n";
 
     system("pause");
     cout << "\n\n";
